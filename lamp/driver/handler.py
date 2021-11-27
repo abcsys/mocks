d33aa541e31aac7785856aa12b0060ec88be996a
silveryfu <silveryfu@gmail.com>
2021-11-26 23:44:13 -0800

    add mock lamp
    Signed-off-by: silveryfu <silveryfu@gmail.com>

---
+++ lamp/driver/handler.py
@@ -0,0 +1,18 @@
+import digi
+import digi.on as on
+
+
+@on.control("power")
+def h0(p):
+    p["status"] = p.get("intent",
+                        p.get("status", "undef"))
+
+
+@on.control("brightness")
+def h1(b):
+    b["status"] = b.get("intent",
+                        b.get("status", "-1"))
+
+
+if __name__ == '__main__':
+    digi.run()
