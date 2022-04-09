bb10537a17ec5c665ebea0a7ffd5138dcbb0e8e2
silveryfu <silveryfu@gmail.com>
2022-04-09 00:02:58 -0700

    fix building occupancy calculation
    Signed-off-by: silveryfu <silveryfu@gmail.com>

--- building/driver/handler.py
+++ building/driver/handler.py
@@ -4,18 +4,19 @@ from digi import on, util
 room_gvr = "mock.digi.dev/v1/rooms"
 
 
-@on.mount
-def update_occupancy_flow(proc_view):
-    rooms = util.get(proc_view, f"mount.'{room_gvr}'", {})
-    num_room = len(rooms)
-    if num_room == 0:
-        digi.util.update(proc_view, "egress.occupancy.pause", True)
-    else:
-        digi.util.update(proc_view, "egress.occupancy.pause", False)
-        digi.util.update(proc_view, "egress.occupancy.flow",
-                         f"occupancy > 0.0 | count := count() | "
-                         f"occupancy:=cast(count, <float64>)/{len(rooms)} |"
-                         f"cut occupancy")
+# @on.mount
+# def update_occupancy_flow(proc_view):
+#     rooms = util.get(proc_view, f"mount.'{room_gvr}'", {})
+#     num_room = len(rooms)
+#     if num_room == 0:
+#         digi.util.update(proc_view, "egress.occupancy.pause", True)
+#     else:
+#         digi.util.update(proc_view, "egress.occupancy.pause", False)
+#         digi.util.update(proc_view, "egress.occupancy.flow",
+#                          f"sort -r event_ts | head | "
+#                          f"occupancy > 0.0 | count := count() | "
+#                          f"occupancy:=cast(count, <float64>)/{len(rooms)} |"
+#                          f"cut occupancy")
 
 
 # @on.pool(in_flow="")
