d65dc131278b7d87d1f2b3250401e554af6c4e57
silveryfu <silveryfu@gmail.com>
2022-07-05 12:01:01 -0700

    update building
    Signed-off-by: silveryfu <silveryfu@gmail.com>

--- building/driver/handler.py
+++ building/driver/handler.py
@@ -3,21 +3,13 @@ import digi.on as on
 import digi.util as util
 
 from digi import dbox
-import random
+dbox.init()
 
 
-@on.mount
-def do_manage(mounts):
-    dbox.manage(mounts)
-
-
-# event
 @dbox.loop
 def event():
-    num_human = random.randint(0, 2)
-    digi.model.patch({
-        "obs": {"num_human": num_human}
-    })
+    num_human = dbox.random.randint(0, 2)
+    digi.model.patch("obs.num_human", num_human)
 
 
 # sim
@@ -27,7 +19,7 @@ def do_obs(sv, mounts):
     names, num_human = list(rooms.keys()), sv.get("num_human", 0)
     if len(names) < 1:
         return
-    picked = set(random.choices(names, k=num_human))
+    picked = set(dbox.random.choices(names, k=num_human))
     for name, room in rooms.items():
         util.update(room, "spec.obs.human_presence", name in picked)
 
